c38e93eab59bf00ea1e8d8afe3a44cf0ff3667d9
ggutu <151479796+ggutu@users.noreply.github.com>
2024-05-04 15:43:28 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "fusisoft-terraform-backend-bucket2"
-    key    = "infra_resources/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "terraform-state-locking"
+    bucket         = "terraform-s3-tfstate"
+    region         = "us-east-2"
+    key            = "ec2-example/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
